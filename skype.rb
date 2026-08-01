6f087e61d14860e46f72812c0fdb77652b6dd75d
Kevin <kevinalimohsin@gmail.com>
2026-08-01 10:59:23 -0700

    Add files via upload

---
+++ skype.rb
@@ -0,0 +1,12 @@
+cask "skype" do
+  version "8.138.0.214"
+  sha256 :no_check
+
+  url "https://dl.download.it/mac/US/skype.dmg?st=Tm1lCRwCuzVP7qRj9F9NBQ&e=1785616826"
+  name "Skype"
+  desc "Classic Skype client from download.it mirror"
+  homepage "https://skype.en.download.it/mac"
+
+  app "Skype.app"
+end
+
